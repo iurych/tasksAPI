b64ac258eaa0e088f3f6913324f8417c22b08e88
iurych <iury.vibe@gmail.com>
2023-09-15 19:42:14 -0300

    feat: add errors handle

--- src/middleware/verifyTitle.js
+++ src/middleware/verifyTitle.js
@@ -1,16 +1,14 @@
 import { Database } from '../database.js';
+import { AppError } from '../errors.js';
 
 const database = new Database();
-console.log(database)
+
 
 export function verifyTaskExist(req, res) {
     const { title } = req.body
     
-    console.log(database.select('tasks'))
-    
-    if(Object.keys(database) > 0) { 
-      const checkTask = database.filter((data) => data.title == title)
-      if (!checkTask[0]) throw new Error({ message: "A task with this title has already been created!" })
-    }
-  
+    const tasks = database.select('tasks')
+    const checkTask = tasks.filter(data => data.title == title)
+
+    if(checkTask) throw new AppError("A task with this title has already been created!", 409)
   }
